6df041be445324001873f1a0f4ad714f88f53b03
roger <roger.herikstad@gmail.com>
2018-02-08 12:49:07 +0800

    Fixes some major bugs in the algorithm

--- src/DemixedPCA.jl
+++ src/DemixedPCA.jl
@@ -14,32 +14,9 @@ type dPCA
 end
 
 """
-Fit a demixed-PCA model to the data in `X`. We have `(d,n1,n2) = size(X)`, where `d` is the dimension and `n1` and `n2` are observations along different dimensions, e.g. realizations (trials) and time, respectively.
-
-Kobak, D., Brendel, W., Constantinidis, C., & Feierstein, C. E. (2016). Demixed principal component analysis of neural population data. eLife. http://doi.org/10.7554/eLife.10989.001
+Perform dPCA by finding an optimal set of weights that minimize the difference betwen Yϕ and FDY.
 """
-function fit(::Type{dPCA},X::Array{Float64,3},labels::Array{Int64,1};maxoutdim=3,λ=0.0)::dPCA
-	ncells,ntrials,nbins = size(X)
-	#get the average firing rate for each cell
-	μ = mean(X, (2,3))
-  Xϕ = zeros(X)
-  ulabels = unique(labels)
-  sort!(ulabels)
-  #marginalize over the labels
-  for (i,l) in enumerate(ulabels)
-    _idx = labels .== l
-    for jj in 1:size(X,1)
-			for kk in 1:size(X,3)
-				_m = mean(X[jj,_idx,kk]- μ[jj])
-				Xϕ[jj,_idx,kk] .= _m
-			end
-    end
-  end
-	Y = reshape(permutedims(X .-μ, [3,2,1]), ntrials*nbins, ncells)'
-	Yϕ = reshape(permutedims(Xϕ, [3,2,1]), ntrials*nbins, ncells)'
-	#marginalize over time
-
-  #solve the ordinary least square problem, i.e. argmin ||Xϕ - XA_OLS||
+function compute_dpca(Y::Matrix{Float64}, Yϕ::Matrix{Float64};maxoutdim=3, λ=0.0)
   if λ == 0.0
     A_OLS = Yϕ/Y
   else
@@ -52,7 +29,42 @@ function fit(::Type{dPCA},X::Array{Float64,3},labels::Array{Int64,1};maxoutdim=3
   #Aq=pp*pp'*A_OLS
   F = pp
   D = pp'*A_OLS
-  return dPCA(F, D,mmc,A_OLS)
+  dPCA(F,D, mmc, A_OLS)
+end
+
+function marginalize(X::Array{Float64,3}, trial_labels::AbstractArray{Int64,1})
+    ncells, ntrials, nbins = size(X)
+    labels = unique(trial_labels)
+    sort!(labels)
+    Xp = zeros(X)
+    for ll in labels
+        tidx = trial_labels.==ll
+        μt = mean(X[:,tidx,:], (2,3)).*ones(ncells,sum(tidx),nbins)
+        Xp[:,tidx,:] = μt 
+    end
+    Xp
+end
+
+"""
+Fit a demixed-PCA model to the data in `X`. We have `(d,n1,n2) = size(X)`, where `d` is the dimension and `n1` and `n2` are observations along different dimensions, e.g. realizations (trials) and time, respectively.
+
+Kobak, D., Brendel, W., Constantinidis, C., & Feierstein, C. E. (2016). Demixed principal component analysis of neural population data. eLife. http://doi.org/10.7554/eLife.10989.001
+"""
+function fit(::Type{dPCA},X::Array{Float64,3},labels::Array{Int64,1};maxoutdim=3,λ=0.0)
+	ncells,ntrials,nbins = size(X)
+	#get the average firing rate for each cell
+	μ = mean(X, (2,3))
+    
+    components = Dict()
+    Y = reshape(permutedims(X, [3,2,1]), ntrials*nbins, ncells)'
+    #time component
+    Yt = reshape(permutedims(mean(X .-μ, 2).*ones(size(X)), [3,2,1]), ntrials*nbins, ncells)'
+    components["time"] = compute_dpca(Y,Yt)
+    #stimulus component
+    Xs = marginalize(X, labels)
+    Ys = reshape(permutedims(Xs, [3,2,1]), ntrials*nbins, ncells)'
+    components["stimulus"] = compute_dpca(Y, Ys)
+    return components
 end
 
 """
@@ -65,5 +77,6 @@ function transform(ppc::dPCA, X::Array{Float64,3})
 	end
 	y
 end
+
 end #module
 
